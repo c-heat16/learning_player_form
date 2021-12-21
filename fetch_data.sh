e2452ef8a89b5f01236a41ee5966e6ddc8b49227
czh <c.heat16@yahoo.com>
2021-12-21 08:18:49 -0500

    Analyze DB after construction

--- fetch_data.sh
+++ fetch_data.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+export DB_FP="../database/mlb.db"
+
 # move to source dir
 cd src
 
 # fetch play-by-play data
 python3 fetch_data.py --statcast T --pitching_by_season F --batting_by_season F --start_year 2015 --end_year 2019 \
-                      --n_pybaseball_workers 3
+                      --n_pybaseball_workers 3 --database_fp $DB_FP
 
 # fetch season-by-season stats
 python3 fetch_data.py --statcast F --pitching_by_season T --batting_by_season T --start_year 1995 --end_year 2019 \
-                      --n_pybaseball_workers 1
+                      --n_pybaseball_workers 1 --database_fp $DB_FP
