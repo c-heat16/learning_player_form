f0c0eaa73a1c4e0e19e3c6bba68e4c38ff2ee5c3
czh <c.heat16@yahoo.com>
2022-01-14 10:00:36 -0500

    Clean up

--- cluster_player_forms_and_plot.sh
+++ cluster_player_forms_and_plot.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 export FORM_DIR="$PWD/out/forms/batter_form_v1"
-#export FORM_DIR="$PWD/out/forms/pitcher_form_v1"
-
 export CLUSTER_OUT_DIR="$PWD/out/form_cluster/batter1_agglom"
-#export CLUSTER_OUT_DIR="$PWD/out/form_cluster/pitcher1_agglom"
-
 export WHOLE_GAME_RECORD_DIR="$PWD/data/whole_game_records/by_season"
+
+#export FORM_DIR="$PWD/out/forms/pitcher_form_v1"
+#export CLUSTER_OUT_DIR="$PWD/out/form_cluster/pitcher1_agglom"
 #export WHOLE_GAME_RECORD_DIR="/home/czh/sata1/learning_player_form/whole_game_records/by_season"
 
 export CLUSTER_TO_PLOT="$CLUSTER_OUT_DIR/mappings/cluster_map_k75.json"
